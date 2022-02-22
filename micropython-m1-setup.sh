4f1b59a0fe388557a6a59a367bf27c6f560f7cd5
paganellif <filippo1paganelli@gmail.com>
2022-02-22 13:51:57 +0100

    add blink led sketch

--- micropython-m1-setup.sh
+++ micropython-m1-setup.sh
@@ -11,6 +11,8 @@ esptool.py --port /dev/cu.usbserial-0001 erase_flash
 wget https://micropython.org/resources/firmware/esp8266-1m-20220117-v1.18.bin
 esptool.py --port /dev/cu.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-20220117-v1.18.bin
 
-# Open Micropython REPL
+# Install serial com tool for REPL
 brew install picocom
-picocom /dev/cu.usbserial-0001 -b115200
+
+# Install Adafruit tool to upload micropython sketch
+pip install adafruit-ampy
--- micropython-m1-upload-sketch.sh
+++ micropython-m1-upload-sketch.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# Pip tool install
-python3 -m pip install --upgrade pip
-pip install esptool
+# Default boot.py esp8266
+# ampy --port /dev/cu.usbserial-0001 get boot.py
 
-# Erase Flash
+  ## This file is executed on every boot (including wake-boot from deepsleep)
+  #import esp
+  ##esp.osdebug(None)
+  #import uos, machine
+  ##uos.dupterm(None, 1) # disable REPL on UART(0)
+  #import gc
+  ##import webrepl
+  ##webrepl.start()
+  #gc.collect()
+
+# Erase Firmware
 esptool.py --port /dev/cu.usbserial-0001 erase_flash
 
 # Flash Firmware
 wget https://micropython.org/resources/firmware/esp8266-1m-20220117-v1.18.bin
 esptool.py --port /dev/cu.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-20220117-v1.18.bin
 
-# Install serial com tool for REPL
-brew install picocom
-
-# Install Adafruit tool to upload micropython sketch
-pip install adafruit-ampy
+# /dev/cu.usbserial-0001 put my-module-imported-from-main.py
+ampy --port /dev/cu.usbserial-0001 put main.py
