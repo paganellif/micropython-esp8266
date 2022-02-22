75d4ed0e9af3476682d5b66fc19d6cd4ff01c6b1
paganellif <filippo1paganelli@gmail.com>
2022-02-22 13:58:58 +0100

    fix scripts

--- micropython-m1-reset.sh
+++ micropython-m1-reset.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Pip tool install
-python3 -m pip install --upgrade pip
-pip install esptool
-
 # Erase Flash
 esptool.py --port /dev/cu.usbserial-0001 erase_flash
 
@@ -11,8 +7,3 @@ esptool.py --port /dev/cu.usbserial-0001 erase_flash
 wget https://micropython.org/resources/firmware/esp8266-1m-20220117-v1.18.bin
 esptool.py --port /dev/cu.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-1m-20220117-v1.18.bin
 
-# Install serial com tool for REPL
-brew install picocom
-
-# Install Adafruit tool to upload micropython sketch
-pip install adafruit-ampy
--- micropython-m1-upload-sketch.sh
+++ micropython-m1-upload-sketch.sh
@@ -13,12 +13,5 @@
   ##webrepl.start()
   #gc.collect()
 
-# Erase Firmware
-esptool.py --port /dev/cu.usbserial-0001 erase_flash
-
-# Flash Firmware
-wget https://micropython.org/resources/firmware/esp8266-1m-20220117-v1.18.bin
-esptool.py --port /dev/cu.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-20220117-v1.18.bin
-
 # /dev/cu.usbserial-0001 put my-module-imported-from-main.py
 ampy --port /dev/cu.usbserial-0001 put main.py
