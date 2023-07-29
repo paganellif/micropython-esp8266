30c794393e01b0190bb793c7e6bd93162f8a9163
Filippo Paganelli <filippo1paganelli@gmail.com>
2023-07-29 09:07:45 +0200

    Update micropython-m1-setup.sh

--- micropython-m1-setup.sh
+++ micropython-m1-setup.sh
@@ -8,8 +8,8 @@ pip install esptool
 esptool.py --port /dev/cu.usbserial-0001 erase_flash
 
 # Flash Firmware
-wget https://micropython.org/resources/firmware/esp8266-1m-20220117-v1.18.bin
-esptool.py --port /dev/cu.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-1m-20220117-v1.18.bin
+wget https://micropython.org/resources/firmware/esp8266-20230426-v1.20.0.bin
+esptool.py --port /dev/cu.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-20230426-v1.20.0.bin
 
 # Install serial com tool for REPL
 brew install picocom
